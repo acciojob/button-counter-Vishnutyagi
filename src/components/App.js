b126d851b07829657a4ec58aefc35e569d25dd8c
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-07-21 13:35:42 +0530

    Solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react'
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+function App() {
   const [cnt,setCnt]=useState(0);
   const clicked=()=>{
-    setCnt(cnt++);
+    setCnt(cnt+1);
   }
   return (
     <div>
-        <p>Button clicked {cnt} times</p>
+        {<p>Button clicked {cnt} times</p>}
         <button onClick={clicked}>Click me</button>
     </div>
-  )
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
